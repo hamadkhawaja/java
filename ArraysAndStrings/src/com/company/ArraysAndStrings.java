618ee431ce80e2789348e15ce3d80f8103ddb5fc
hamadkhawaja <hamad.jillani@gmail.com>
2015-04-29 21:09:07 -0700

    Arrays And Strings

--- ArraysAndStrings/src/com/company/ArraysAndStrings.java
+++ ArraysAndStrings/src/com/company/ArraysAndStrings.java
@@ -26,4 +26,26 @@ public class ArraysAndStrings {
 
         return retVal;
     }
+
+    public void setZeros(int [][]matrix) {
+        boolean[] row = new boolean[matrix.length];
+        boolean[] column = new boolean[matrix[0].length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = true;
+                    column[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (row[i] || column[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
 }
--- ArraysAndStrings/src/com/company/Main.java
+++ ArraysAndStrings/src/com/company/Main.java
@@ -7,5 +7,22 @@ public class Main {
 	// write your code here
         ArraysAndStrings arr = new ArraysAndStrings();
         System.out.println(arr.stringConpressString("aaabbbcAAbC"));
+
+        int [][]array = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = 1;
+            }
+        }
+
+        array[1][1] = 0;
+        arr.setZeros(array);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
